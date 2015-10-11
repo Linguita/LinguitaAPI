4d3179706e58f6e1bb2030775fc3fc085e026b83
Carlos Rojas <carlos.sadhu@gmail.com>
2015-10-10 23:52:30 -0500

    Remove json_csrf protection

--- env.rb
+++ env.rb
@@ -8,6 +8,8 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 use Rack::Session::Cookie, :secret => 'change_me'
 
+set :protection, :except => [:json_csrf]
+
 use OmniAuth::Builder do
   # For additional provider examples please look at 'omni_auth.rb'
   provider :google_oauth2, "908297860097-cvrlevd7kvguuc9r45bunvq647lddfkc.apps.googleusercontent.com", "5J1vHDizqFRcOjM505l2UgCa"
